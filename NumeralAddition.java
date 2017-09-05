fa703b41ff7a01428b0b370d5c653a3874ea2b1e
Carly H <carlyhublou@gmail.com>
2017-09-04 22:50:35 -0400

    Update NumeralAddition.java

--- NumeralAddition.java
+++ NumeralAddition.java
@@ -1,3 +1,6 @@
+//Assignment description: Write a program that reads an integer between 0 and 1000 and adds all the
+//digits in the integer. For example, if an integer is 932, the sum of all its digits is 14.
+
 package a1_hubloucarly;
 
 import java.util.Scanner;
