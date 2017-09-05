3428f449a59df4b53b5cc6ff8b33f7542af9960b
Carly H <carlyhublou@gmail.com>
2017-09-04 22:46:26 -0400

    Add files via upload

---
+++ MonetaryUnits.java
@@ -0,0 +1,45 @@
+package monetaryunits;
+
+import static java.lang.Math.round;
+import java.util.Scanner;
+
+public class MonetaryUnits {
+
+    public static void main(String[] args) {
+        //Create new scanner
+        Scanner input = new Scanner(System.in);
+        
+        //Prompt user for dollar amount and get amount
+        System.out.println("Enter a dollar amount in dollars and cents (e.g., 15.43)");
+        double totalAmount = input.nextDouble();
+        
+        //Find total cents
+        int remainingAmount = (int)Math.round(totalAmount * 100);
+                
+        //Find dollars
+        int dollars = remainingAmount / 100;
+        remainingAmount = remainingAmount % 100;
+                
+        //Find quarters
+        int totalQuarters = remainingAmount / 25;
+        remainingAmount = remainingAmount % 25;
+             
+        //Find dimes
+        int totalDimes = remainingAmount / 10;
+        remainingAmount = remainingAmount % 10;
+                
+        //Find nickels
+        int totalNickels = remainingAmount / 5;
+        remainingAmount = remainingAmount % 5;
+                
+        //Find pennies
+        int totalPennies = remainingAmount;
+        
+        //Print result
+        System.out.println("You have " + dollars + " dollars, " + totalQuarters + 
+                " quarters, " + totalDimes + " dimes, " + totalNickels + " nickels, and " + 
+                totalPennies + " pennies.");
+                
+    }
+    
+}
