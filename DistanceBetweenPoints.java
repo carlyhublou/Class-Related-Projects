17cb90bc274c3033c85db35f2504fe902a903139
Carly H <carlyhublou@gmail.com>
2017-09-04 23:11:08 -0400

    Update DistanceBetweenPoints.java

--- DistanceBetweenPoints.java
+++ DistanceBetweenPoints.java
@@ -1,3 +1,5 @@
+//Problem description: Write a program that prompts the user to enter two points (x1, y1) and (x2, y2) and displays their distance.
+
 package a1_hubloucarly;
 
 import java.util.Scanner;
